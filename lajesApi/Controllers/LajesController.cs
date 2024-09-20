63ba4737ca9376269472cd4fdb2a7709e3498e4d
BrunoHunoff <brunohunoff@gmail.com>
2024-09-20 19:16:35 -0300

    ..

--- lajesApi/Controllers/LajesController.cs
+++ lajesApi/Controllers/LajesController.cs
@@ -1,16 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 
-public static class LajesController {
-    
-    public static void AddLajesEndpoints (this WebApplication app) {
-        var lajesEndpoint = app.MapGroup ("lajes");
+public static class LajesController
+{
+
+    public static void AddLajesEndpoints(this WebApplication app)
+    {
+        var lajesEndpoint = app.MapGroup("lajes");
 
         //GET ALL
         lajesEndpoint.MapGet("", async ([FromServices] LajeRepository lajeRepository) =>
             await lajeRepository.GetAllLajes());
 
         //GET ID
-        lajesEndpoint.MapGet("{id:guid}", async ([FromRoute] Guid id, LajeRepository lajeRepository) => {
+        lajesEndpoint.MapGet("{id:guid}", async ([FromRoute] Guid id, LajeRepository lajeRepository) =>
+        {
             var laje = await lajeRepository.GetLajeById(id);
 
             if (laje is null) return Results.NotFound();
@@ -19,7 +22,8 @@ public static class LajesController {
         });
 
         //POST
-        lajesEndpoint.MapPost("", async (AddLajesRequest request, [FromServices] LajeRepository lajeRepository) => {
+        lajesEndpoint.MapPost("", async (AddLajesRequest request, [FromServices] LajeRepository lajeRepository) =>
+        {
             var novaLaje = new Laje(request.Name, request.Price);
 
             await lajeRepository.AddLajeAsync(novaLaje);
@@ -27,16 +31,20 @@ public static class LajesController {
         });
 
         //PUT
-        lajesEndpoint.MapPut("{id:guid}", async ([FromRoute] Guid id, [FromBody] Laje novaLaje, [FromServices] LajeRepository lajeRepository) => {
-        try{
-            var laje = await lajeRepository.UpdateLaje(id, novaLaje);
-        return Results.Ok(laje);
-        } catch (KeyNotFoundException e) {
-            return Results.NotFound(e.Message);
-        }
+        lajesEndpoint.MapPut("{id:guid}", async ([FromRoute] Guid id, [FromBody] Laje novaLaje, [FromServices] LajeRepository lajeRepository) =>
+        {
+            try
+            {
+                var laje = await lajeRepository.UpdateLaje(id, novaLaje);
+                return Results.Ok(laje);
+            }
+            catch (KeyNotFoundException e)
+            {
+                return Results.NotFound(e.Message);
+            }
 
-        
-});
+
+        });
 
 
         //DELETE
