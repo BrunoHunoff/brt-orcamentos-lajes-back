4d1ed8f8bf110a4cce18b32c3183b9997b6a354f
BrunoHunoff <brunohunoff@gmail.com>
2024-09-20 16:13:05 -0300

    Adiciona metodo get id

--- lajesApi/Controllers/LajesController.cs
+++ lajesApi/Controllers/LajesController.cs
@@ -5,12 +5,21 @@ public static class LajesController {
     public static void AddLajesEndpoints (this WebApplication app) {
         var lajesEndpoint = app.MapGroup ("lajes");
 
-
+        //GET ALL
         lajesEndpoint.MapGet("", async ([FromServices] LajeRepository lajeRepository) =>
             await lajeRepository.GetAllLajes());
 
+        //GET ID
+        lajesEndpoint.MapGet("{id:Guid}", async ([FromRoute] Guid id, LajeRepository lajeRepository) => {
+            var laje = await lajeRepository.GetLajeById(id);
 
-        lajesEndpoint.MapPost("", async (AddLajesRequest request, AppDbContext dbContext,[FromServices] LajeRepository lajeRepository) => {
+            if (laje == null) return Results.NotFound();
+
+            return Results.Ok(laje);
+        });
+
+
+        lajesEndpoint.MapPost("", async (AddLajesRequest request, [FromServices] LajeRepository lajeRepository) => {
             var novaLaje = new Laje(request.Name, request.Price);
 
             var lajeExiste = await lajeRepository.LajeExists(novaLaje.Name);
