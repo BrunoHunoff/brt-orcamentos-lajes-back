ad7328e7013a66b26bda42de276a3eacea0c6343
BrunoHunoff <brunohunoff@gmail.com>
2024-09-25 12:41:05 -0300

    add BudgetSlab model

--- lajesApi/Models/Budget.cs
+++ lajesApi/Models/Budget.cs
@@ -1,13 +1,14 @@
 public class Budget {
     public int Id { get; init; }
-    public int ClientId { get; init; }
+    public int CostumerId { get; private set; }
     public double Footage { get; private set; }
     public double Value { get; private set; }
     public string City { get; private set; }
     public string State { get; private set; }
+    public List<BudgetSlab> Slabs { get; private set; } = new List<BudgetSlab>();
 
-    public Budget(int clientId, double footage, double value, string city, string state) {
-        ClientId = clientId;
+    public Budget(int costumerId, double footage, double value, string city, string state) {
+        CostumerId = costumerId;
         Footage = footage;
         Value = value;
         City = city;
---
+++ lajesApi/Models/BudgetSlab.cs
@@ -0,0 +1,8 @@
+public class BudgetSlab {
+    public int Id { get; init; }
+    public int SlabId { get; private set; }
+    public int BudgetId { get; private set; }
+    public int SlabsNumber { get; private set; }
+    public double Length { get; private set; }
+    public double Width { get; private set; }
+}
